b6e5d8b7c44d4092ae6fe8cfa51cf72e74c27644
diligentdpf <1852300415@qq.com>
2016-04-26 22:44:30 +0800

    俄罗斯方块小游戏
    俄罗斯方块小游戏

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+	public static void main(String args[]){
+		Map<Object,Object> map=new HashMap<Object,Object>();
+		Map<Object,Object> map1 = new HashMap<Object,Object>();
+		System.out.println(map);
+	}
+}
